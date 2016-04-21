bdb30843d69b23e925a074f8456786dcc9349901
zckop <304400612@qq.com>
2016-04-21 21:46:22 +0800

    修改

--- php/application/bool/Model/Model.class.php
+++ php/application/bool/Model/Model.class.php
@@ -1,11 +1,5 @@
 <?php
-/****
-燕十八 公益PHP讲堂
 
-论  坛: http://www.zixue.it
-微  博: http://weibo.com/Yshiba
-YY频道: 88354001
-****/
 defined('ACC')||exit('ACC Denied');
 class Model {
     protected $table = NULL; // 是model所控制的表
