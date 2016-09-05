5d115f3444a9fae662f0aee3ec11dd07511ebbdc
zckop <304400612@qq.com>
2016-09-05 21:28:49 +0800

    无限极分类

--- php/algorithm/wxjfl.php
+++ php/algorithm/wxjfl.php
@@ -103,4 +103,104 @@ function wxjfl_iterative_one(){
 
 }
 
+
+
+/*
+
+// 变种
+$a = new test();
+$a->index();
+class test{
+
+
+    private $list = array();
+
+
+    public function index()
+    {
+
+        $this->init();
+
+        $outlist=array();
+        $outlist['l']=0;
+        $this->getChild(0, $outlist);
+        print_r($outlist);
+
+    }
+
+
+
+    private function getChild($puid, &$out)
+    {
+        $out['child'] = array();
+
+        foreach ($this->list as $value) {
+
+            if ($value['parent'] == $puid) {
+                $this->count++;
+                $value['l'] = $out['l']+1;
+                $out['child'][$value['id']] = $value;
+                $this->getChild($value['id'], $out['child'][$value['id']]);
+            }
+        }
+
+
+    }
+
+
+    private function init()
+    {
+
+
+        $tmp  = array(
+            array('id'=>1,'model'=>1,'parent'=>0),
+            array('id'=>2,'model'=>1,'parent'=>1),
+            array('id'=>3,'model'=>1,'parent'=>2),
+            array('id'=>4,'model'=>1,'parent'=>3),
+            array('id'=>5,'model'=>2,'parent'=>0),
+            array('id'=>6,'model'=>2,'parent'=>5),
+            array('id'=>7,'model'=>1,'parent'=>6),
+            array('id'=>8,'model'=>1,'parent'=>7)
+        );
+
+        $del_mode = array();
+        foreach($tmp as $key=>$value)
+        {
+            if($value['model']==2)
+            {
+                $del_mode[$value['id']] = $value['parent'];
+                unset($tmp[$key]);
+            }
+        }
+
+
+        while(1)
+        {
+            $run = 0;
+            foreach($tmp as $k=>$v)
+            {
+                if(in_array($v['parent'],array_keys($del_mode)))
+                {
+                    $tmp[$k]['parent'] = $del_mode[$v['parent']];
+                    $run=1;
+                }
+            }
+            if(!$run)
+            {
+                break;
+            }
+        }
+
+        $this->list=$tmp;
+
+
+    }
+
+
+}
+
+
+*/
+
+
 ?>
